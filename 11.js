4619a702ce4ba96ff0c9a4eaa766bfa944426ee5
sonkan <alexander.k.gertz@gmail.com>
2017-12-11 12:04:22 +0100

    Passing solution for day 11 part two

--- 11.js
+++ 11.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 let puzzleInput = fs.readFileSync('input11.txt', {encoding: 'utf-8'});
 let directions = puzzleInput.split(',');
 let position = {x: 0, y: 0}; // Origo
+let positions = [];
 let steps;;
 
 // x, y
@@ -18,7 +19,12 @@ let directionMap = {
 directions.forEach(direction => {
     position.x = position.x + directionMap[direction].x;
     position.y = position.y + directionMap[direction].y;
+    positions.push({x: position.x, y: position.y});
 });
 
 steps = Math.abs(position.x) + Math.abs(position.y);
 console.log("Part one: " + steps);
+
+let furthestSteps = Math.max(...positions.map(pos => Math.abs(pos.x) + Math.abs(pos.y)));
+
+console.log("Part two: " + furthestSteps);
