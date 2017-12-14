84019da7f0e134d922dd542fb5b94acc637d0be7
Alexander Gertz <alexander.k.gertz@gmail.com>
2017-12-14 19:06:34 +0100

    Beatiful mess: passing solution for day 12 part two

--- 12.js
+++ 12.js
@@ -3,63 +3,51 @@ const fs = require('fs');
 let puzzleInput = fs.readFileSync('input12.txt', {encoding: 'utf-8'});
 
 let rows = puzzleInput.split('\n');
-let connections = [0];
+rows = rows.map(row => {
+    return [parseInt(row.split('<->')[0]), [...row.split('<->')[1].split(',')]];
+});
 let groups = [];
 
-function findConnections  (rows, connections) {
+function findConnections  (connections) {
     let foundAll = false;
+    let newConnections = [];
 
     while (!foundAll) {
-        let newConnections = [];
-
         rows.forEach(row => {
-            let c = row.split('<->');
-            if (connections.indexOf(parseInt(c[0])) > -1) {
-                if (parseInt(c[0]) == 757 || parseInt(c[0]) == 338) {
-                    console.log(...c[1].split(',').map(n => parseInt(n)));
-                }
-                newConnections.push(...c[1].split(',').map(n => parseInt(n)));
+            if (connections.indexOf(row[0]) > -1) {
+                row[1].forEach(n => {
+                    newConnections.push(parseInt(n));
+                });
             }
         });
 
 
-        let totalConnections = [...connections, ...newConnections];
+        let totalConnections = [...new Set([...connections, ...newConnections])];
 
-        if ([...new Set(connections)].length == [...new Set(totalConnections)].length) {
+        if ([...new Set(connections)].length == totalConnections.length) {
             foundAll = true;
             if (newConnections.length) {
-                groups.push([...new Set(totalConnections)]);
+                groups.push(totalConnections);
             }
-            return [...new Set(totalConnections)];
         }
-        connections = [...new Set(totalConnections)];
+        connections = totalConnections;
     }
-}
-
-let foundConnections = findConnections(rows, [0]);
-console.log("Part one: " + foundConnections.length);
-
-function remainingConnections (rows, foundConnections)  {
-    rows.filter((row, index) => {
-        let c = row.split('<->');
-        let matchConnections = [parseInt(c[0]), ...c[1].split(',').map(n => parseInt(n))];
-        matchConnections.forEach(connection => {
-            if ([].concat(...groups).indexOf(connection) > -1) {
+    groups[groups.length - 1].forEach(n => {
+        rows.forEach((rn, index) => {
+            if (rn[0] == n) {
                 rows.splice(index, 1);
             }
         })
     });
-
-    return rows;
 }
 
-while (rows.length) {
-    rows = remainingConnections(rows, foundConnections);
-    if (!rows.length) continue;
-    let nextGroup = [parseInt(rows[0].split('<->')[0])];
-    foundConnections = findConnections(rows, nextGroup);
+findConnections([0]);
+console.log("Part one: " + groups[0].length);
 
+let amIDone = false;
+while (!amIDone) {
+    findConnections([rows[rows.length - 1][0]]);
+    if (!rows.length) amIDone = true;
 }
-
 
 console.log("Part two: " + groups.length);
